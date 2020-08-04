e304795de3fa7e9792667d83288bc9f9df99faa7
sethbassett <seth.bassett@gmail.com>
2020-08-04 19:51:24 -0400

    testing apt pt 2

--- apt/apt-pg-11-postgis-3.sh
+++ apt/apt-pg-11-postgis-3.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt bionic-pgdg main" >> /etc/apt/sources.list'
+##############################################################################
+#####  INSTALL POSTGRESQL ####################################################
+##############################################################################
+# ADD APT REPOSITORY
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt bionic-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 
+apt update
 
-sudo apt install -y postgresql-11 
-sudo apt install -y postgresql-11 postgresql-server-dev-11 postgresql-client-11
-sudo apt install -y postgresql-11-postgis-3 postgresql-11-postgis-3-scripts postgis
-
+# install postgresql and create superuser
+# do NOT install proj, geos, gdal, or postgis
+apt install -y postgresql-11 postgresql-server-dev-11 postgresql-client-11
+apt install -y libpq-dev libxml2-dev libxml2-utils
+apt install postgresql-11-ogr-fdw
 sudo -u postgres createuser --superuser root
-
-sudo apt install -y postgresql-11-ogr-fdw
+##############################################################################
+##############################################################################
+##############################################################################
