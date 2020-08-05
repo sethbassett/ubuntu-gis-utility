6c754e0cf5359de91870cae2957b7add7ea6352b
sethbassett <seth.bassett@gmail.com>
2020-08-05 02:55:39 +0000

    fix postgres config file issue

--- config/config-pg-11-postgis-3.sh
+++ config/config-pg-11-postgis-3.sh
@@ -12,26 +12,22 @@ cp \
   /etc/postgresql/11/main/pg_hba.conf.bak
 
 cp \
-  /etc/postgresql/11/main/environment 
+  /etc/postgresql/11/main/environment \
   /etc/postgresql/11/main/environment.bak
 
-rm /etc/postgresql/11/main/postgresql.conf
-rm /etc/postgresql/11/main/pg_hba.conf
-rm /etc/postgresql/11/main/environment
-
-mv \
-  ubuntu-gis-utility/config/conf/pg_environment \
+cp \
+  ~/ubuntu-gis-utility/config/conf/pg_environment \
   /etc/postgresql/11/main/environment
 
-mv \
-  ubuntu-gis-utility/config/conf/pg_hba.conf \
+cp \
+  ~/ubuntu-gis-utility/config/conf/pg_hba.conf \
   /etc/postgresql/11/main/pg_hba.conf
 
-mv \
-  ubuntu-gis-utility/config/conf/postgresql.conf \
+cp \
+  ~/ubuntu-gis-utility/config/conf/postgresql.conf \
   /etc/postgresql/11/main/postgresql.conf
   
-ufw allow 5432
+#ufw allow 5432
 
 systemctl start postgresql
 
