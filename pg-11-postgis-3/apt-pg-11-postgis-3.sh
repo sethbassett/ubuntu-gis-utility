e270af8cd69b0dfa287106a430062bd42e5e885b
sethbassett <seth.bassett@gmail.com>
2020-08-05 19:11:24 -0400

    update readme.md

--- pg-11-postgis-3/apt-pg-11-postgis-3.sh
+++ pg-11-postgis-3/apt-pg-11-postgis-3.sh
@@ -2,6 +2,8 @@
 ##############################################################################
 #####  INSTALL POSTGRESQL ####################################################
 ##############################################################################
+# https://trac.osgeo.org/postgis/wiki/UsersWikiPostGIS24UbuntuPGSQL10Apt
+
 # ADD APT REPOSITORY
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt bionic-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
@@ -10,9 +12,20 @@ apt update
 
 # install postgresql and create superuser
 # do NOT install proj, geos, gdal, or postgis
-apt install -y postgresql-11 postgresql-server-dev-11 postgresql-client-11
-apt install -y libpq-dev libxml2-dev libxml2-utils
-apt install -t postgresql-11-ogr-fdw
+# postgres
+apt install -y \
+  postgresql-11 \
+  postgresql-server-dev-11 \
+  postgresql-client-11
+
+apt install -y \
+  postgresql-11-postgis-3 \
+  postgresql-11-postgis-3-scripts \
+  postgresql-11-pgrouting
+apt install -y \
+ postgresql-11-ogr-fdw
+
+
 sudo -u postgres createuser --superuser root
 ##############################################################################
 ##############################################################################
